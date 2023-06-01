4bb4688a3ca9f95f996b14b0cb19c16329d1ed6f
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-06-01 17:50:49 +0500

    product-description.html page

--- js/main.js
+++ js/main.js
@@ -22,3 +22,15 @@ $('.category-slick').slick({
     }]
 })
 
+$().ready(function () {
+    let t = $('#back-top')
+    $(window).on('scroll', function () {
+        $scrollTop = $(window).scrollTop(), t = $('#back-top'), $scrollTop > 350 ? t.addClass('visible') : t.removeClass('visible')
+    
+        $scrollTop > 0 ? $('.es-navbar').addClass('es-navbar-fixed') : $('.es-navbar').removeClass('es-navbar-fixed')
+        })
+        $htmlBody = $('html, body'), t.length && t.on('click', function (t) {
+        $htmlBody.animate({scrollTop: 0}, 200), t.preventDefault()
+    }   
+)
+})
