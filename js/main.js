507814131fa838838174c2c2abd5a3446f093bc7
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-06-06 22:01:44 +0500

    team page

--- js/main.js
+++ js/main.js
@@ -34,3 +34,51 @@ $().ready(function () {
     }   
 )
 })
+
+$('.main-slick').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    prevArrow: '<button class="slick-prev main-slick-arrow" aria-label="Previous" type="button"><img src="img/ic_left_arrow.svg" class="img-fluid"/></button>',
+    nextArrow: '<button class="slick-next main-slick-arrow" aria-label="Next" type="button"><img src="img/ic_right_arrow.svg" class="img-fluid"/></button>',
+    responsive: [{
+    breakpoint: 1024, settings: {
+        slidesToShow: 2, slidesToScroll: 1, infinite: true, dots: true
+    }
+    }, {
+    breakpoint: 600, settings: {
+        slidesToShow: 4, slidesToScroll: 1
+    }
+    }, {
+    breakpoint: 480, settings: {
+        slidesToShow: 1, slidesToScroll: 1
+    }
+    }]
+})
+
+$('.main-slick-two').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    prevArrow: '<button class="slick-prev-two slick-arrow-two" aria-label="Previous" type="button"><img src="img/ic_left_arrow.svg" class="img-fluid"/></button>',
+    nextArrow: '<button class="slick-next-two slick-arrow-two" aria-label="Next" type="button"><img src="img/ic_right_arrow.svg" class="img-fluid"/></button>',
+    responsive: [{
+    breakpoint: 1024, settings: {
+        slidesToShow: 6, slidesToScroll: 1, infinite: true, dots: true
+    }
+    }, {
+    breakpoint: 600, settings: {
+        slidesToShow: 4, slidesToScroll: 1
+    }
+    }, {
+    breakpoint: 480, settings: {
+        slidesToShow: 2, slidesToScroll: 1
+    }
+    }]
+})
