38c486be03635c324589c7ae70985e58ce36dd8d
Sanam Khudoyberdieva <khudoyberdievasanamkhon@gmail.com>
2023-06-09 11:53:53 +0500

    about pages

--- js/main.js
+++ js/main.js
@@ -39,18 +39,18 @@ $('.main-slick').slick({
     dots: false,
     infinite: false,
     speed: 300,
-    slidesToShow: 2,
+    slidesToShow: 1.7,
     slidesToScroll: 1,
     swipeToSlide: true,
     prevArrow: '<button class="slick-prev main-slick-arrow" aria-label="Previous" type="button"><img src="img/ic_left_arrow.svg" class="img-fluid"/></button>',
     nextArrow: '<button class="slick-next main-slick-arrow" aria-label="Next" type="button"><img src="img/ic_right_arrow.svg" class="img-fluid"/></button>',
     responsive: [{
     breakpoint: 1024, settings: {
-        slidesToShow: 2, slidesToScroll: 1, infinite: true, dots: true
+        slidesToShow: 1.7, slidesToScroll: 1, infinite: true, dots: true
     }
     }, {
     breakpoint: 600, settings: {
-        slidesToShow: 4, slidesToScroll: 1
+        slidesToShow: 1, slidesToScroll: 1
     }
     }, {
     breakpoint: 480, settings: {
@@ -66,15 +66,88 @@ $('.main-slick-two').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
     swipeToSlide: true,
-    prevArrow: '<button class="slick-prev-two slick-arrow-two" aria-label="Previous" type="button"><img src="img/ic_left_arrow.svg" class="img-fluid"/></button>',
-    nextArrow: '<button class="slick-next-two slick-arrow-two" aria-label="Next" type="button"><img src="img/ic_right_arrow.svg" class="img-fluid"/></button>',
+    prevArrow: '<button class="slick-prev-two slick-arrow-two last-arrow1" aria-label="Previous" type="button"><img src="img/arrow-down-black.svg" class="img-fluid"/></button>',
+    nextArrow: '<button class="slick-next-two slick-arrow-two last-arrow2" aria-label="Next" type="button"><img src="img/arrow-down-black.svg" class="img-fluid"/></button>',
     responsive: [{
     breakpoint: 1024, settings: {
         slidesToShow: 6, slidesToScroll: 1, infinite: true, dots: true
     }
     }, {
     breakpoint: 600, settings: {
+        slidesToShow: 1, slidesToScroll: 1
+    }
+    }, {
+    breakpoint: 480, settings: {
+        slidesToShow: 1, slidesToScroll: 1
+    }
+    }]
+})
+
+$('.main-two-category-slick').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1.1,
+    swipeToSlide: true,
+    prevArrow: '<button class="slick-prev main-slick-arrow slick-two" aria-label="Previous" type="button"><img src="img/ic_left_arrow.svg" class="img-fluid"/></button>',
+    nextArrow: '<button class="slick-next main-slick-arrow slick-two two-s" aria-label="Next" type="button"><img src="img/ic_right_arrow.svg" class="img-fluid"/></button>',
+    responsive: [{
+    breakpoint: 1024, settings: {
+        slidesToShow: 4, slidesToScroll: 1, infinite: true, dots: true
+    }
+    }, {
+    breakpoint: 600, settings: {
         slidesToShow: 4, slidesToScroll: 1
+    }
+    }, {
+    breakpoint: 480, settings: {
+        slidesToShow: 1.1, slidesToScroll: 1
+    }
+    }]
+})
+
+
+$('.main-slick-3').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2.7,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    prevArrow: '<button class="slick-prev main-slick-arrow" aria-label="Previous" type="button"><img src="img/ic_left_arrow.svg" class="img-fluid"/></button>',
+    nextArrow: '<button class="slick-next main-slick-arrow" aria-label="Next" type="button"><img src="img/ic_right_arrow.svg" class="img-fluid"/></button>',
+    responsive: [{
+    breakpoint: 1024, settings: {
+        slidesToShow: 2.7, slidesToScroll: 1, infinite: true, dots: true
+    }
+    }, {
+    breakpoint: 600, settings: {
+        slidesToShow: 4, slidesToScroll: 1
+    }
+    }, {
+    breakpoint: 480, settings: {
+        slidesToShow: 1, slidesToScroll: 1
+    }
+    }]
+})
+
+$('.main-slick-dif').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    prevArrow: '<button class="slick-prev-two slick-arrow-two last-arrow1" aria-label="Previous" type="button"><img src="img/arrow-down-black.svg" class="img-fluid"/></button>',
+    nextArrow: '<button class="slick-next-two slick-arrow-two last-arrow2" aria-label="Next" type="button"><img src="img/arrow-down-black.svg" class="img-fluid"/></button>',
+    responsive: [{
+    breakpoint: 1024, settings: {
+        slidesToShow: 3, slidesToScroll: 1, infinite: true, dots: true
+    }
+    }, {
+    breakpoint: 600, settings: {
+        slidesToShow: 3, slidesToScroll: 1
     }
     }, {
     breakpoint: 480, settings: {
